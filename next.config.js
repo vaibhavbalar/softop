790279eae6edbe6f25e37047b13505f8bd46715c
vaibhavbalar <104752761+vaibhavbalar@users.noreply.github.com>
2022-11-24 20:03:13 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
-/**const nextConfig = {
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig */
-
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/about',
-        destination: '/',
-        permanent: true,
-      },
-    ]
-  },
-}
+module.exports = nextConfig
